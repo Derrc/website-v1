d74d029fb92b32a6dced2a1217359fa783d52fd7
Derrc <derrickskim@gmail.com>
2024-03-07 18:13:44 -0500

    edit about text

--- frontend/src/components/AboutSection.vue
+++ frontend/src/components/AboutSection.vue
@@ -29,23 +29,23 @@ onUnmounted(() => {
   <div>
     <h1 class="font-mn mb-2 text-5xl">Derrick Kim</h1>
     <span id="typed" class="font-mn h-8 block text-lg" />
-    <div class="about-container p-8 my-6">
-      <h1 class="font-mn text-4xl mb-4">About Me</h1>
-      <p class="font-inter text-base mb-2 leading-loose">
+    <ul class="about-container p-8 my-6 text-sm sm:text-base list-disc">
+      <h1 class="font-mn text-3xl sm:text-4xl mb-4">About Me</h1>
+      <li class="font-inter mb-2 leading-loose ml-4">
         Hello! I'm Derrick and I am a student at Northeastern University
         majoring in Computer Science with a minor in Math. I'm an aspiring
         developer with a focus in full-stack development. My current favorite
         languages and frameworks to work with are React, Vue, and Typescript.
-      </p>
-      <p class="font-inter text-base mb-2 leading-loose">
+      </li>
+      <li class="font-inter mb-2 leading-loose ml-4">
         I love to constantly learn and pick up new skills! Whether its a new
         language, tool, or concept, I try my best to learn something new
         everyday. There is so much to learn in the software development space
         and that is one of many aspects I love about it. My current goals are to
-        dig deeper into cloud technologies, pickup a new programming language,
-        and build my own software as a service.
-      </p>
-    </div>
+        dig deeper into cloud technologies and system design, pickup a new
+        programming language, and build my own software as a service.
+      </li>
+    </ul>
     <ContactSection />
   </div>
 </template>
